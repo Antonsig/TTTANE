dfd3cbbd67220a97486d888ac663a1e5949ae02d
bibz <bjarni.thor.palsson@gmail.com>
2012-11-25 23:46:20 +0000

    GameLogicTest
    Lagfært

--- TTTANEtest/GameLogicTest.cs
+++ TTTANEtest/GameLogicTest.cs
@@ -18,7 +18,7 @@ namespace TTTANEtest
         /// Prufar getGameBoard fall
         /// </summary>
         [TestMethod()]
-        public void TestSetMove()
+        public void TestGetGameBoard()
         {
             
             String[] test = new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
